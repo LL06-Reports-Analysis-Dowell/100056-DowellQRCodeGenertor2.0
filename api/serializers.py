205bf30695da940be51ab7872b638acd0f44898f
Eugene <eugeneregi2019@gmail.com>
2023-04-06 07:44:28 +0300

    Code factoring

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,4 @@ from rest_framework import serializers
 
 class DoWellQrCodeSerializer(serializers.Serializer):
     logo = serializers.SerializerMethodField()
-    # product_name = serializers.CharField(max_length=128,allow_null=False, allow_blank=False)
     link = serializers.URLField(max_length=128,allow_null=False, allow_blank=False)
-    # company_id = serializers.CharField(max_length=128,allow_null=False, allow_blank=False)
-    # create_by = serializers.CharField(max_length=128,allow_null=False, allow_blank=False)
