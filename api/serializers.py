71bf75981ac529db3db220efaa3abbac145a5a0d
Eugene <eugeneregi2019@gmail.com>
2023-04-12 22:18:47 +0300

    Added a logo inside the qrcode

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 
 class DoWellQrCodeSerializer(serializers.Serializer):
-    logo = serializers.SerializerMethodField()
+    logo = serializers.ImageField(allow_null=True, allow_empty_file=False)
     product_name = serializers.CharField(max_length=128,allow_null=False, allow_blank=False)
     link = serializers.URLField(max_length=128,allow_null=False, allow_blank=False)
     company_id = serializers.CharField(max_length=128,allow_null=False, allow_blank=False)
     create_by = serializers.CharField(max_length=128,allow_null=False, allow_blank=False)
+
+
+
+    # logo = serializers.ImageField(allow_null=True, allow_empty_file=False)
--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from tkinter import Image
 import qrcode
 import base64
 from io import BytesIO
+import logging
 from api.serializers import *
+from PIL import Image
 from rest_framework import status
 from rest_framework.views import APIView
 from database.database_management import *
@@ -19,28 +21,49 @@ class codeqr(APIView):
     def post(self, request):
         link = request.data.get("link")
         product_name = request.data.get("product_name")
-        logo = request.data.get('logo')
+        logo = request.FILES.get('logo')
         create_by = request.data.get("create_by")
         company_id = request.data.get("company_id")
 
+        # logo_image = Image.open("image/logo.png")
+        logo_image = Image.open(BytesIO(base64.b64decode(logo)))
+        logo_image = logo_image.convert("RGBA")
 
-        # logo_data = decode_base64(logo)
-        # logo_image = Image.open
+        print("Logo image size:", logo_image.size)
+
+        # Create the QR code image
         qr_code = qrcode.QRCode(version=1, 
-                            error_correction=qrcode.constants.ERROR_CORRECT_L, 
-                            box_size=10, border=4)
+                                error_correction=qrcode.constants.ERROR_CORRECT_L, 
+                                box_size=10, border=4)
         qr_code.add_data(link)
         qr_code.make(fit=True)
         img_qr = qr_code.make_image(fill_color="black", back_color="white")
+        print("QR code size:", img_qr.size)
 
+        # Calculate the position to place the logo
+        logo_size = min(img_qr.size[0], img_qr.size[1]) * 25 // 100 # Change this value to adjust the size of the logo
+        logo_x = (img_qr.size[0] - logo_size) // 2
+        logo_y = (img_qr.size[1] - logo_size) // 2
+
+        # Resize the logo to the desired size
+        logo_image = logo_image.resize((logo_size, logo_size), resample=Image.LANCZOS)
+
+        # Paste the logo on the QR code image
+        img_qr.paste(logo_image, (logo_x, logo_y))
+
+        # Encode the logo image to base64
+        buffer = BytesIO()
+        logo_image.save(buffer, format="PNG")
+        logo_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+        # Encode the QR code image to base64
         buffer = BytesIO()
         img_qr.save(buffer, 'PNG')
         img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
-
         field = {
             "link": link,
-            "logo": logo,
+            "logo": logo_base64,
             "create_by" : create_by,
             "company_id": company_id,
             "product_name":  product_name,
