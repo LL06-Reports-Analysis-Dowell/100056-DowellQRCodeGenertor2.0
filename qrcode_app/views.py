5356a2b090d1707e5b8520c53936b11f01e1f89b
Eugene <eugeneregi2019@gmail.com>
2023-05-08 14:25:52 +0300

    Qrcode

--- qrcode_app/views.py
+++ qrcode_app/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# hash value for qr id
-# link 
-# link for activate
-# name of qrcode
-# description
-# number of qr code
-# color 
-# logo image
+# hash value for qr id (Automatic generataion)
+# link (done)
+# link for activate(Update api)
+#Product name
+# (Product name to be added to the updated Api)
+# description(Add an input in the Updated api)
+# number of qr code (Pass the as aparam)
+# color (First step api)
+# logo image (Upated APi set default formart png)
 # enable functionality of qr code
-# status 
-# type of qr code
-# product name
+# status (Is Active)
+# type of qr code(Use the figma design type)
+# product name ()
 
 # search functionality --->
+
+# On the first step when creating bulk Qrcodes we will include  only the number is needed and  color
