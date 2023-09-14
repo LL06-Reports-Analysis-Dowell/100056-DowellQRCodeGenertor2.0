32b7b6a6adbc722a0a4061be9feba743e9a75a03
vader <pythongeeko@gmail.com>
2023-09-14 16:37:06 +0300

    allow-cors

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,9 +140,6 @@ INTERSERVER_URL = 'https://dowellfileuploader.uxlivinglab.online/uploadfiles/upl
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-
-
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
