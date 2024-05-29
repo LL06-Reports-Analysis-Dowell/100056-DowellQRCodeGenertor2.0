fdfab8b84c68c461951e7cef8b971944e585c64b
umair271 <malikumair2701@gmail.com>
2024-05-29 22:35:20 +0500

    fix the put master Api

--- V6MasterLink/views.py
+++ V6MasterLink/views.py
@@ -28,67 +28,69 @@ class QRCodeAPIView(APIView):
             return Response({"error": "QR code not found"}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request):
-            num_qrcodes = request.data.get('num_qrcodes', 1)
-            qrcode_type = request.data.get("qrcode_type")
-            logo = request.FILES.get('logo')
-            logo_size = int(request.data.get("logo_size", "20"))
-            qrcode_color = request.data.get('qrcode_color', "#000000")
-            created_by = request.data.get("created_by")
-            lat= request.data.get("lat", "None")
-            long = request.data.get("long", "None")
-            is_active = request.data.get("is_active", False)
-            playStoreLink = 'https://play.google.com/store/apps/details?id=com.dowellqrcodescanner.app&pli=1'
-            redirect_link = request.data.get("redirect_link")
-            qrcodes_created = []
+        num_qrcodes = request.data.get('num_qrcodes', 1)
+        qrcode_type = request.data.get("qrcode_type")
+        logo = request.FILES.get('logo')
+        logo_size = int(request.data.get("logo_size", "20"))
+        qrcode_color = request.data.get('qrcode_color', "#000000")
+        created_by = request.data.get("created_by")
+        lat = request.data.get("lat", "None")
+        long = request.data.get("long", "None")
+        is_active = request.data.get("is_active", False)
+        playStoreLink = 'https://play.google.com/store/apps/details?id=com.dowellqrcodescanner.app&pli=1'
+        qrcodes_created = []
+        qrcode_id = ""
+        logo_file = None
+        if logo:
+            logo_file = logo.read()
 
-            logo_file = None
-            if logo:
-                logo_file = logo.read()  # Converts InMemoryUploadedFile to bytes
+        num_qrcodes = int(num_qrcodes) if int(num_qrcodes) > 0 else 1
+        common_id = str(uuid.uuid4())
+        for _ in range(num_qrcodes):
+            logo_url = None
+            if logo_file:
+                logo_url = upload_image_to_interserver(logo_file, logo.name)
+            qrcode_id = f'22-{str(uuid.uuid4())}'
 
-            num_qrcodes = int(num_qrcodes) if int(num_qrcodes) > 0 else 1
+            field = {
+                "qrcode_id": qrcode_id,
+                "generate_master_QR_code_id": common_id,
+                "logo_size": logo_size,
+                "qrcode_color": qrcode_color,
+                "created_by": created_by,
+                "lat": lat,
+                "long": long,
+                "is_active": is_active,
+                "qrcode_type": qrcode_type,
+                'playStoreLink': playStoreLink,
+                'redirect_link': None
+            }
 
-            for _ in range(num_qrcodes):
-                logo_url = None
-                if logo_file:
-                    logo_url = upload_image_to_interserver(logo_file, logo.name)
-                qrcode_id = f'22-{str(uuid.uuid4())}'
+            serializer, field = qrcode_type_defination(qrcode_id, is_active, qrcode_type, request, qrcode_color,
+                                                       logo, field, logo_url)
 
-                field = {
-                    "qrcode_id": qrcode_id,
-                    "logo_size": logo_size,
-                    "qrcode_color": qrcode_color,
-                    "created_by": created_by,
-                    "lat": lat,
-                    "long": long,
-                    "is_active": is_active,
-                    "qrcode_type": qrcode_type,
-                    'playStoreLink': playStoreLink,
-                    'redirect_link': redirect_link
-                }
-
-                serializer, field = qrcode_type_defination(qrcode_id, is_active, qrcode_type, request, qrcode_color,
-                                                           logo, field, logo_url)
-                print(field)
-                if serializer.is_valid():
-                    try:
-                        print(Apikey)
-                        response = QR_code_datacube_data_insertion(Apikey, DATABASE_NAME, QR_CODE_COLLECTION_NAME,
-                                                                   field)
-                        response = json.loads(response)
-                        print(request)
-                        if response['success']:
-                            qrcodes_created.append(response['data']['inserted_id'])
-                        else:
-                            return Response({"error": response.get('message')},
-                                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-                    except Exception as e:
-                        return Response({"error": f"An error occurred: {str(e)}"},
+            if serializer.is_valid():
+                try:
+                    print(Apikey)
+                    response = QR_code_datacube_data_insertion(Apikey, DATABASE_NAME, QR_CODE_COLLECTION_NAME,
+                                                               field)
+                    response = json.loads(response)
+                    print(request)
+                    if response['success']:
+                        qrcodes_created.append(field)
+                    else:
+                        return Response({"error": response.get('message')},
                                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-                else:
-                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                except Exception as e:
+                    return Response({"error": f"An error occurred: {str(e)}"},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({"response": f"{num_qrcodes} QR codes created successfully.", "qrcodes": qrcodes_created},
-                            status=status.HTTP_201_CREATED)
+        return Response(
+            {"response": f"{num_qrcodes} QR codes created successfully.", "generate_master_QR_code_id": common_id,
+             "qrcodes_data": qrcodes_created},
+            status=status.HTTP_201_CREATED)
     
     def put(self, request, qrcode_id):
        
@@ -105,8 +107,6 @@ class QRCodeAPIView(APIView):
             "playStoreLink": request.data.get("playStoreLink"),
             "redirect_link": request.data.get("redirect_link"),
             "product_name": request.data.get("product_name"),
-            "title": request.data.get("title"),
-            "website": request.data.get("website")
         }
 
         update_data = {k: v for k, v in update_data.items() if v is not None}
@@ -167,6 +167,10 @@ class MasterQRCodeAPIView(APIView):
     def post(self, request):
         id = request.data.get('generate_master_QR_code_id')
         data = {"generate_master_QR_code_id": id}
+        email = request.data.get('email')
+        name = request.data.get('name')
+        location = request.data.get('location')
+        description = request.data.get('description')
 
         response = datacube_data_retrieval(Apikey, DATABASE_NAME, QR_CODE_COLLECTION_NAME, data)
         response = json.loads(response)
@@ -175,14 +179,18 @@ class MasterQRCodeAPIView(APIView):
             return Response({"error": response.get('message')}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         qrcode_list = response["data"]
-        list_qr_id = [{"qr_id": i["qrcode_id"]} for i in qrcode_list]
+        list_qr_id = [{"qr_id": i["qrcode_id"], "qrcode_image_url": i["qrcode_image_url"], "link": i["link"]} for i in qrcode_list]
 
         master_qr_code_id = f'11-{str(uuid.uuid4())}'
         field = {
             "master_qr_code_id": master_qr_code_id,
             "master_qr_code_link": None,
+            "name": name,
+            "location": location,
+            "description": description,
+            "is_used": False,
             "qr_code_ids": list_qr_id,
-            "is_used": False
+
         }
 
         link = f"https://www.qrcodereviews.uxlivinglab.online/{master_qr_code_id}"
@@ -201,14 +209,7 @@ class MasterQRCodeAPIView(APIView):
         response = json.loads(response)
 
         if response['success']:
-            fields = {
-                "master_qr_code_id": master_qr_code_id,
-                "master_qr_code_link": link,
-                "is_used": False,
-                "master_qrcode_image_url": qr_code_url,
-                "qr_code_data": qrcode_list
-            }
-            return Response({"response": "Master QR code created successfully.", "master_qrcode": fields},
+            return Response({"response": "Master QR code created successfully.", "master_qrcode": field},
                             status=status.HTTP_201_CREATED)
         else:
             return Response({"error": response.get('message')}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -257,6 +258,7 @@ class MasterQRCodeAPIView(APIView):
                                     status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             all_active = all(qr_code['is_active'] for qr_code in qr_code_data_list)
 
+            field = {}
             if all_active:
                 field = {
                     "master_qr_code_id": master_qr_code_id,
@@ -272,7 +274,17 @@ class MasterQRCodeAPIView(APIView):
                     return Response({"error": "Failed to update master QR code"},
                                     status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-            return Response(qr_code_data_list)
+            update_data = {
+                "name": request.data.get("name"),
+                "location": request.data.get("location"),
+                "description": request.data.get("location"),
+                "redirect_link": request.data.get("redirect_link"),
+            }
+            update_data = {k: v for k, v in update_data.items() if v is not None}
+            master_update_response = datacube_data_update(Apikey, DATABASE_NAME, MASTER_QR_CODE_COLLECTION_NAME,
+                                                          field, update_data)
+            if master_update_response['success']:
+                return Response(qr_code_data_list)
         else:
             return Response({"error": "No QR code are available"}, status=status.HTTP_404_NOT_FOUND)
         
