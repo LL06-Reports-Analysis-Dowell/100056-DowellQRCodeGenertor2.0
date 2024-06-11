d51700b8310c36031f76c109e7d69f1da626692f
umair271 <malikumair2701@gmail.com>
2024-06-11 23:17:24 +0500

    fix the redirect issue

--- V6MasterLink/urls.py
+++ V6MasterLink/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import QRCodeAPIView, MasterQRCodeAPIView, CloneQRCodeAPIView, QRCodeDataAPIView
+from .views import QRCodeAPIView, MasterQRCodeAPIView, CloneQRCodeAPIView, QRCodeDataAPIView, redirect_link
 
 urlpatterns = [
     path('qrcodes/', QRCodeAPIView.as_view(), name='qrcode-list-create'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('activate-qr-code/<str:master_qr_code_id>/', MasterQRCodeAPIView.as_view(), name='activate-qr-code'),
     path('master-qrcodes/update/<str:master_qr_code_id>/', MasterQRCodeAPIView.as_view(), name='update-master-qr'),
     #path('clone-qrcode/', CloneQRCodeAPIView.as_view(), name='clone-qrcode'),
-    path('redirect_link/<str:qrcode_id>/', views.redirect_link, name='qr_code_view'),
+    path('redirect_link/<str:qrcode_id>/', redirect_link, name='qr_code_view'),
     path('qrcode-data/', QRCodeDataAPIView.as_view(), name='qrcode-data'),
     path('qrcode-data/<str:qrcode_id>/', QRCodeDataAPIView.as_view(), name='qrcode-data'),
 ]
